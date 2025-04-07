5e5d0f4c3ae0daf814f7a82b04886396fe1be28b
Vaibhav Garg <vaibhav.garg@salesforce.com>
2025-04-07 15:58:29 -0700

    Fix OAuth callback handling by properly including it in the build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env) => {
   const isLocalMode = env && env.local;
@@ -74,6 +75,11 @@ module.exports = (env) => {
         template: './src/commands.html',
         filename: 'commands.html',
         chunks: ['commands']
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'public', to: '' }
+        ]
       })
     ],
     devServer: {
