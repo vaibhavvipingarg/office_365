8237598959c0273bafb754602359e71ab9a22706
Vaibhav Garg <vaibhav.garg@salesforce.com>
2025-04-07 15:01:32 -0700

    Update Salesforce credentials in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Salesforce configuration for GitHub Pages deployment
 window.SalesforceConfig = {
-  loginUrl: "https://login.salesforce.com",
-  clientId: "your_client_id_here", // Replace with your actual Salesforce client ID
+  loginUrl: "https://sdb42com6.test13.my.pc-rnd.salesforce.com",
+  clientId: "3MVG9kfeuo6xCm.oW1tEpQ87E09RhbDFSYvGtCPH73D4C3CpSFmUv152XQ3gvB4GgcFvt3p1kXwJme1slmhm3",
   redirectUri: "https://vaibhavvipingarg.github.io/office_365/oauth/callback/"
 }; 
