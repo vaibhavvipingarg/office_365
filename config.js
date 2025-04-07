b6f261a751787af06f62fd00b3bd83cc35e2394e
Vaibhav Garg <vaibhav.garg@salesforce.com>
2025-04-07 16:08:37 -0700

    fix: update OAuth callback URL to include office_365 path

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 window.SalesforceConfig = {
   loginUrl: "https://sdb42com6.test13.my.pc-rnd.salesforce.com",
   clientId: "3MVG9kfeuo6xCm.oW1tEpQ87E09RhbDFSYvGtCPH73D4C3CpSFmUv152XQ3gvB4GgcFvt3p1kXwJme1slmhm3",
-  redirectUri: "https://vaibhavvipingarg.github.io/oauth-callback.html"
+  redirectUri: "https://vaibhavvipingarg.github.io/office_365/oauth-callback.html"
 }; 
